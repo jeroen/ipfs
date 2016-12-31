2950e417e6b9a6560a97cbd055376bd28c2366bb
Jeroen Ooms <jeroenooms@gmail.com>
2016-12-31 15:19:53 +0100

    Add ipfs_download function

--- R/ipfs.R
+++ R/ipfs.R
@@ -10,8 +10,28 @@
 #' @references \url{https://ipfs.io/ipfs/QmR7GSQM93Cx5eAg6a6yRzNde1FQv7uL6X1o4k7zrJa3LX/ipfs.draft3.pdf}
 #' ipfs_version()
 #' ipfs_get('QmYwAPJzv5CZsnA625s3Xf2nemtYgPpHdWEz79ojWnPbdG')
+#' ipfs_data('QmYwAPJzv5CZsnA625s3Xf2nemtYgPpHdWEz79ojWnPbdG/readme')
 ipfs_get <- function(key){
   ipfs_json("object/get", arg = key)
+}
+
+#' @export
+#' @rdname ipfs
+ipfs_data <- function(key){
+  ipfs_raw("object/data", arg = key)
+}
+
+#' @export
+#' @rdname ipfs
+#' @param path filename to save download.
+#' @examples ipfs_download('QmR7GSQM93Cx5eAg6a6yRzNde1FQv7uL6X1o4k7zrJa3LX/ipfs.draft3.pdf')
+#' utils::browseURL('ipfs.draft3.pdf')
+ipfs_download <- function(key, path = NULL){
+  if(!length(path))
+    path <- basename(key)
+  buf <- ipfs_data(key)
+  writeBin(buf, path)
+  return(path)
 }
 
 #' @export
---
+++ readme.md
@@ -0,0 +1,13 @@
+# IPFS for R
+
+## Installation
+
+For now you need to manually run IPFS daemon to use this package:
+
+```
+brew install ipfs
+ipfs init
+ipfs daemon
+```
+
+See `?ipfs` for examples.
