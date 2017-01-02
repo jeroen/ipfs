9279f8bb2db135c0c8cb41ad95aceb50f4a66f70
Jeroen Ooms <jeroenooms@gmail.com>
2017-01-02 13:16:05 +0100

    Add more bindings

--- R/ipfs.R
+++ R/ipfs.R
@@ -6,19 +6,49 @@
 #' @name ipfs
 #' @rdname ipfs
 #' @param key a multihash key
+#' @references IPFS api: \url{https://ipfs.io/docs/api/}
+#' Draft paper: \url{https://ipfs.io/ipfs/QmR7GSQM93Cx5eAg6a6yRzNde1FQv7uL6X1o4k7zrJa3LX/ipfs.draft3.pdf}
 #' @examples # From 'getting started'
-#' @references \url{https://ipfs.io/ipfs/QmR7GSQM93Cx5eAg6a6yRzNde1FQv7uL6X1o4k7zrJa3LX/ipfs.draft3.pdf}
-#' ipfs_version()
+#' ipfs_info()
 #' ipfs_get('QmYwAPJzv5CZsnA625s3Xf2nemtYgPpHdWEz79ojWnPbdG')
 #' ipfs_data('QmYwAPJzv5CZsnA625s3Xf2nemtYgPpHdWEz79ojWnPbdG/readme')
+#'
 ipfs_get <- function(key){
   ipfs_json("object/get", arg = key)
 }
 
 #' @export
 #' @rdname ipfs
+ipfs_ls <- function(key){
+  ipfs_json("ls", arg = key)
+}
+
+#' @export
+#' @rdname ipfs
 ipfs_data <- function(key){
-  ipfs_raw("object/data", arg = key)
+  buf <- ipfs_raw("object/data", arg = key)
+  if(identical(buf, as.raw(c(0x08, 0x01))))
+    stop("Not a data block")
+  return(buf)
+}
+
+#' @export
+#' @rdname ipfs
+ipfs_swarm <- function(){
+  list(
+    local = ipfs_json("swarm/addrs/local")$Strings,
+    peers = ipfs_json("swarm/peers")$Strings
+  )
+}
+
+#' @export
+#' @rdname ipfs
+ipfs_stats <- function(){
+  list(
+    bitswap = ipfs_json("stats/bitswap"),
+    repo = ipfs_json("stats/repo"),
+    bw = ipfs_json("stats/bw")
+  )
 }
 
 #' @export
@@ -36,15 +66,32 @@ ipfs_download <- function(key, path = NULL){
 
 #' @export
 #' @rdname ipfs
-ipfs_version <- function(){
-  ipfs_json("version")
+ipfs_info <- function(){
+  list(
+    version = ipfs_json("version"),
+    id = ipfs_json("id")
+  )
 }
 
 #' @export
 #' @rdname ipfs
 ipfs_webui <- function(){
-  ipfs_version() #check that demon is online
+  ipfs_info() #check that demon is online
   utils::browseURL(ipfs_url("webui"))
+}
+
+#' @export
+#' @rdname ipfs
+ipfs_commands <- function(){
+  print_tree <- function(x, indent){
+    opts <- unlist(sapply(x$Options, function(y){y$Names[[1]]}))
+    optstr <- ifelse(length(opts), paste("[", paste0("--", opts, collapse = ", "), "]"), " ")
+    cat(sprintf("%s- %s %s\n", strrep(' ', indent), x$Name, optstr))
+    lapply(x$Subcommands, print_tree, indent = indent + 2)
+  }
+  buf <- ipfs_raw("commands", enc = "json")
+  data <- jsonlite::fromJSON(rawToChar(buf), simplifyVector = FALSE)
+  invisible(lapply(data[[2]], print_tree, indent = 2))
 }
 
 ipfs_raw <- function(command, ...){
