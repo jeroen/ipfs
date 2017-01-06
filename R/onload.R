6ace233a09cbb8dfa873a87ddb8d760ec689a81c
Jeroen Ooms <jeroenooms@gmail.com>
2017-01-06 15:29:27 +0100

    Placeholders

--- R/onload.R
+++ R/onload.R
@@ -3,7 +3,15 @@
 }
 
 .onAttach <- function(libname, pkgname){
-
+  if(has_ipfs()){
+    pid <- sys::exec_with_pid("ipfs", c("daemon", "--init"))
+    reg.finalizer(environment(.onLoad), function(...){
+      cat("stopping ipfs...\n")
+      tools::pskill(pid)
+    }, onexit = TRUE)
+  } else {
+    packageStartupMessage("Main 'ipfs' executable not found")
+  }
 }
 
 has_ipfs <- function(){
